a349d48fc24c41430e10b0fe84f4e9a82023924e
Kyle Stubbs <kylestubbs@cs.ucsb.edu>
2020-04-24 16:03:52 -0700

    main should be working

--- main.cpp
+++ main.cpp
@@ -6,14 +6,9 @@
 using namespace std;
 
 int main(int argv, char** argc) {
-	/*if(argv < 2){
-		cout << "Please provide 1 file name" << endl;
-		return 1; 
-	}*/ //eventually uss array to read input commands
 
 	ifstream wordList;
 	string line;
-	cout << argv << " arguments" << endl;	
 	wordList.open("PA1_dataset.txt");
 	if (!wordList) {
 		cout << "could not open file";
@@ -27,9 +22,46 @@ int main(int argv, char** argc) {
 		bt.insertWord(line, true);
 	}
 
-	bt.deleteWord("broadcasting", bt.getRoot(), false);
-	bt.deleteWord("hospitalize", bt.getRoot(), false);
-	bt.deleteWord("britts", bt.getRoot(), false);
-	bt.deleteWord("runner", bt.getRoot(), false);
-	bt.rangeSearch("hospitalization", "hot");
+	int i = 0; 
+	while ( (int)argc[1][i] != 78 ) {
+		if ( argc[1][i] == 's' ) {
+			i += 7;
+			string str = "";
+			while ( (int)argc[1][i] > 96 )
+				str += argc[1][i++];
+			ht.searchWord(str);
+			bt.searchWord(str);
+		}
+		else if ( argc[1][i] == 'i' ) {
+			i += 7;
+			string str = "";
+			while ( (int)argc[1][i] > 96 )
+				str += argc[1][i++];
+			ht.insertWord(str, false);
+			bt.insertWord(str, false);
+		}
+		else if ( argc[1][i] == 'd' ) {
+			i += 7;
+			string str = "";
+			while ( (int)argc[1][i] > 96 )
+				str += argc[1][i++];
+			ht.deleteWord(str);
+			bt.deleteWord(str, bt.getRoot(), false);
+		}
+		else if ( argc[1][i] == 'r' ) {
+			i += 13;
+			string start = "";
+			while ( (int)argc[1][i] > 96 )
+				start += argc[1][i++];
+			i += 4;
+			string end = "";
+			while ( (int)argc[1][i] > 96 )
+				end += argc[1][i++];
+			ht.rangeSearch(start, end);
+			bt.rangeSearch(start, end);
+		}
+		if ( argc[1][i] == ',' )
+			i++;
+		i++;
+	}
 }
